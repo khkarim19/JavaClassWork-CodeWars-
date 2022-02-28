e0bf9887ecfe39e11ff2366f66dba5030c387145
khkarim19 <khkarim@mail.ru>
2022-02-28 12:58:22 +0300

    Задание 2 (Преобразовать число в строку)

--- src/com/karim/Main.java
+++ src/com/karim/Main.java
@@ -3,12 +3,13 @@ package com.karim;
 public class Main {
 
     public static void main(String[] args) {
-        
+        int n = 100;
+        String f = Integer.toString(n);
     }
 
     // Task 1 "Complete the function that takes a non-negative integer n as input,
 // and returns a list of all the powers of 2 with the exponent ranging from 0 to n (inclusive)."
-    static class Kata {
+    static class Kata1 {
         public static long[] powersOfTwo(int n) {
             long[] result = new long[n + 1];
             for (int i = 0; i <= n; i++) {
@@ -18,3 +19,12 @@ public class Main {
         }
     }
 }
+//We need a function that can transform a number into a string.
+//What ways of achieving this do you know?
+
+class Kata2 {
+    public static String numberToString(int num) {
+        return Integer.toString(num);
+    }
+}
+
