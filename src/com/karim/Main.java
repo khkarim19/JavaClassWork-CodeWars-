98bee559ea9aa4a223a6644df5492d19388a505c
khkarim19 <khkarim@mail.ru>
2022-03-14 12:13:44 +0300

    Задание 3 (Сколько нечетных чисел меньших n)

--- src/com/karim/Main.java
+++ src/com/karim/Main.java
@@ -2,14 +2,24 @@ package com.karim;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int n = 100;
-        String f = Integer.toString(n);
+    private static Object IllegalArgumentException;
+
+    public static void main(String[] args) throws Throwable {
+        int n = 5;
+        int m = 20;
+        if (m <= 0) {
+            throw (Throwable) IllegalArgumentException;
+        }
+        int result = 0;
+        for (int i = 0; i <= m; i = i + n) {
+            result = result + i;
+        }
+        System.out.println(result);
     }
 
     // Task 1 "Complete the function that takes a non-negative integer n as input,
 // and returns a list of all the powers of 2 with the exponent ranging from 0 to n (inclusive)."
-    static class Kata1 {
+    class Kata1 {
         public static long[] powersOfTwo(int n) {
             long[] result = new long[n + 1];
             for (int i = 0; i <= n; i++) {
@@ -19,12 +29,24 @@ public class Main {
         }
     }
 }
-//We need a function that can transform a number into a string.
+// Task 2
+// We need a function that can transform a number into a string.
 //What ways of achieving this do you know?
 
 class Kata2 {
     public static String numberToString(int num) {
         return Integer.toString(num);
+    }
+}
+//Given a number n, return the number of positive odd numbers below n
+class Kata3 {
+
+    public static int oddCount(int n) {
+        if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return (n - 1) / 2;
+        }
     }
 }
 
